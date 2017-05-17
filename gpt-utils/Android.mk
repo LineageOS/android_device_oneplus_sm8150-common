54e6eda429e9d50671685835fa2b55cb23107b50
Logan Chien <loganchien@google.com>
2017-05-17 15:36:58 +0800

    sm8150-common: libgptutils: Replace libsparse.so with libz.so
    This commit replaces sparse_crc32() with crc32() from libz.so because
    libsparse.so won't be vendor available.
    
    Bug: 38303202
    Test: android builds
    Change-Id: Ib318da74b5d55f729cbd40d9a62da53e6316fe7b

--- gpt-utils/Android.mk
+++ gpt-utils/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := bootable/recovery \
 		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
+LOCAL_SRC_FILES := gpt-utils.cpp
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_SHARED_LIBRARIES := libz
 LOCAL_MODULE := libgptutils
 include $(BUILD_STATIC_LIBRARY)
 
@@ -13,9 +14,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := bootable/recovery \
 		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
+LOCAL_SRC_FILES := gpt-utils.cpp
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_SHARED_LIBRARIES += liblog libcutils
+LOCAL_SHARED_LIBRARIES += liblog libcutils libz
 LOCAL_MODULE := libgptutils
 LOCAL_COPY_HEADERS_TO := gpt-utils/inc
 LOCAL_COPY_HEADERS := gpt-utils.h

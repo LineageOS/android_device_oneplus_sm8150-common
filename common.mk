e57d870d4083bc23b8579cc7117a3484d4d6de31
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    sm8150-common: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- common.mk
+++ common.mk
@@ -248,6 +248,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common

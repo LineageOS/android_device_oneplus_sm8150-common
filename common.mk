3e4c8d38013b8eb1368c1b5a23e62638b294ff7b
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    sm8150-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- common.mk
+++ common.mk
@@ -21,8 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B
 AB_OTA_UPDATER := true

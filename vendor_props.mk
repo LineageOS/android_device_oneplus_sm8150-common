0092550d2807c55f8af57f779a950e86241cb22f
LuK1337 <priv.luk@gmail.com>
2019-02-02 19:34:23 +0100

    sm8150-common: Add debug IMS props to workaround CarrierConfig restrictions
    We support IMS features on carriers not listed in the CarrierConfig,
    so let's use the props to enable it everywhere.
    
    Change-Id: Id3ad71d522a22048f7ad86d89695ad262361632d

--- vendor_props.mk
+++ vendor_props.mk
@@ -99,3 +99,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.product.device=guacamole \
 	vendor.product.manufacturer=oneplus \
 	ro.vendor.nfc.notify_tp=1 \
+	persist.dbg.volte_avail_ovr=1 \
+	persist.dbg.vt_avail_ovr=1 \
+	persist.dbg.wfc_avail_ovr=1

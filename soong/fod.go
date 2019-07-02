aa2a51329d2a76ab4652085a635d354b8a072d49
Peter Cai <peter@typeblog.net>
2019-07-02 18:28:12 +0800

    sm8150-common: fod: prettify soong variables

--- soong/fod.go
+++ soong/fod.go
@@ -9,10 +9,10 @@ import (
 func deviceFlags(ctx android.BaseContext) []string {
     var cflags []string
 
-    var config = ctx.AConfig().VendorConfig("oneplusMsmnileFodPlugin")
-    var posX = strings.TrimSpace(config.String("posX"))
-    var posY = strings.TrimSpace(config.String("posY"))
-    var size = strings.TrimSpace(config.String("size"))
+    var config = ctx.AConfig().VendorConfig("ONEPLUS_MSMNILE_FOD")
+    var posX = strings.TrimSpace(config.String("POS_X"))
+    var posY = strings.TrimSpace(config.String("POS_Y"))
+    var size = strings.TrimSpace(config.String("SIZE"))
 
     cflags = append(cflags, "-DFOD_POS_X=" + posX, "-DFOD_POS_Y=" + posY, "-DFOD_SIZE=" + size)
     return cflags

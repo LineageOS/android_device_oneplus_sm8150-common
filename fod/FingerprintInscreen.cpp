5989333d361a3b113baa6570e3c02b4db35fd8e8
LuK1337 <priv.luk@gmail.com>
2019-07-02 12:14:55 +0200

    sm8150-common: fod: Drop netlink related code
    Change-Id: I90531b222b5f67845a0e8d240953cebacbb9860c

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -20,9 +20,6 @@
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
 #include <fstream>
-#include <linux/netlink.h>
-#include <sys/socket.h>
-#include <thread>
 
 #define OP_ENABLE_FP_LONGPRESS 3
 #define OP_DISABLE_FP_LONGPRESS 4
@@ -37,16 +34,6 @@
 #define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/op_friginer_print_hbm"
 #define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
 
-#define GF_NETLINK_ROUTE 25
-#define GF_NETLINK_TP_TOUCHDOWN 4
-#define GF_NETLINK_TP_TOUCHUP 5
-
-#define SIFP_NETLINK_ROUTE 30
-#define SIFP_NETLINK_TP_TOUCHDOWN 7
-#define SIFP_NETLINK_TP_TOUCHUP 8
-
-#define MAX_NETLINK_PAYLOAD 1024
-
 namespace vendor {
 namespace lineage {
 namespace biometrics {
@@ -54,6 +41,8 @@ namespace fingerprint {
 namespace inscreen {
 namespace V1_0 {
 namespace implementation {
+
+using namespace std::chrono_literals;
 
 /*
  * Write value to path and close file.
@@ -76,79 +65,6 @@ static T get(const std::string& path, const T& def) {
 FingerprintInscreen::FingerprintInscreen() {
     this->mVendorFpService = IVendorFingerprintExtensions::getService();
     this->mVendorDisplayService = IOneplusDisplay::getService();
-
-    const auto listenToNetlinkMsgs = [this](int protocol, int msgDown, int msgUp) {
-        auto sock_fd = socket(PF_NETLINK, SOCK_RAW, protocol);
-
-        if (sock_fd < 0) {
-            LOG(ERROR) << "Failed to open socket";
-            return;
-        }
-
-        sockaddr_nl src_addr{};
-        src_addr.nl_family = AF_NETLINK;
-        src_addr.nl_pid = getpid();
-
-        sockaddr_nl dest_addr{};
-        dest_addr.nl_family = AF_NETLINK;
-        dest_addr.nl_pid = 0;
-        dest_addr.nl_groups = 0;
-
-        if (bind(sock_fd, reinterpret_cast<sockaddr *>(&src_addr), sizeof(src_addr)) == -1) {
-            LOG(ERROR) << "Failed to bind socket";
-            return;
-        }
-
-        auto nlh = reinterpret_cast<nlmsghdr *>(malloc(NLMSG_SPACE(MAX_NETLINK_PAYLOAD)));
-        memset(nlh, 0, NLMSG_SPACE(MAX_NETLINK_PAYLOAD));
-        nlh->nlmsg_len = NLMSG_SPACE(MAX_NETLINK_PAYLOAD);
-        nlh->nlmsg_pid = getpid();
-        nlh->nlmsg_flags = 0;
-
-        iovec iov{};
-        iov.iov_base = nlh;
-        iov.iov_len = nlh->nlmsg_len;
-
-        msghdr msg{};
-        msg.msg_name = &dest_addr;
-        msg.msg_namelen = sizeof(dest_addr);
-        msg.msg_iov = &iov;
-        msg.msg_iovlen = 1;
-
-        // Send message to kernel
-        sendmsg(sock_fd, &msg, 0);
-
-        while (true) {
-            // Receive message from kernel
-            recvmsg(sock_fd, &msg, 0);
-
-            auto msgId = *reinterpret_cast<int8_t *>(NLMSG_DATA(nlh));
-
-            if (msgId == msgDown) {
-                if (mCallback != nullptr) {
-                    Return<void> ret = mCallback->onFingerDown();
-                    if (!ret.isOk()) {
-                        LOG(ERROR) << "FingerDown() error: " << ret.description();
-                    }
-                }
-            } else if (msgId == msgUp) {
-                if (mCallback != nullptr) {
-                    Return<void> ret = mCallback->onFingerUp();
-                    if (!ret.isOk()) {
-                        LOG(ERROR) << "FingerUp() error: " << ret.description();
-                    }
-                }
-            }
-        }
-    };
-
-    std::thread([=] {
-        listenToNetlinkMsgs(GF_NETLINK_ROUTE, GF_NETLINK_TP_TOUCHDOWN, GF_NETLINK_TP_TOUCHUP);
-    }).detach();
-
-    std::thread([=] {
-        listenToNetlinkMsgs(SIFP_NETLINK_ROUTE, SIFP_NETLINK_TP_TOUCHDOWN, SIFP_NETLINK_TP_TOUCHUP);
-    }).detach();
 }
 
 Return<void> FingerprintInscreen::onStartEnroll() {

ed85f3127e02c49052626e4adccf80237da783e4
Ashwin R C <ashwin2001achu@gmail.com>
2019-10-11 14:33:04 +0000

    sm8150-common: add Oneplus7T Series to build guards
    Change-Id: I1571c08626708657028a9b95ef1f1f16d18e4b3d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -38,7 +38,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "guacamole guacamoleb guacamoleg"
+write_headers "guacamole guacamoleb guacamoleg hotdog hotdogb"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

58d5ca25c3d2d87394c9454bfea5f62f95964134
Peter Cai <peter@typeblog.net>
2019-10-12 12:34:21 +0800

    sm8150-common: sync vendor security patch with OOS 10
    Change-Id: Ia157b1d7868f0a431bf988e4b4b0b095656da853

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RIL_VARIANT := caf
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-08-05
+VENDOR_SECURITY_PATCH := 2019-09-05
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk

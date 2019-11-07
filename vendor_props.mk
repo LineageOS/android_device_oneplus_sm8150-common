76593a54e5c9a6225a4730c6c45a9a08a734f870
1nfrag <sil3ntcll@gmail.com>
2019-11-07 13:05:08 +0800

    sm8150-common: Enable BT A2DP offload
    Change-Id: I3c7120f5f29605a939d0ca78054894e2cafcca65

--- vendor_props.mk
+++ vendor_props.mk
@@ -48,8 +48,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    ro.bluetooth.a2dp_offload.supported=true \
     vendor.qcom.bluetooth.soc=cherokee
 
 # Crypto

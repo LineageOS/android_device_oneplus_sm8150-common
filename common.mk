8f2f4aef07e37e664504363747dc1a8066a8589e
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-13 21:45:50 -0600

    sm8150-common: Enable config_vibrateOnIconAnimation
    * Need those sexy vibes.
    
    Change-Id: If5671135879f6f4069d0ed881b41b0a243890349

--- common.mk
+++ common.mk
@@ -307,7 +307,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
-    OPlusFrameworksResCommon
+    OPlusFrameworksResCommon \
+    OPlusSystemUIResCommon
 
 # Power
 PRODUCT_PACKAGES += \

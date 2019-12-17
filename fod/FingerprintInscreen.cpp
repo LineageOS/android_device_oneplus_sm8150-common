3621940f676014b478611bce0b404fe668be02a0
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-12-17 04:13:25 +0000

    sm8150-common: fod: Add optional HBM toggle
    * Needed for 7/7T/7T Pro.
    
    Change-Id: Ia5b9c5e9551bf8142e063c656eb93617c727f7ac

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -84,7 +84,9 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 Return<void> FingerprintInscreen::onPress() {
     this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
     this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+#if FOD_HBM
     set(HBM_ENABLE_PATH, 1);
+#endif
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
     return Void();
@@ -93,7 +95,9 @@ Return<void> FingerprintInscreen::onPress() {
 Return<void> FingerprintInscreen::onRelease() {
     this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+#if FOD_HBM
     set(HBM_ENABLE_PATH, 0);
+#endif
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -106,7 +110,9 @@ Return<void> FingerprintInscreen::onShowFODView() {
 Return<void> FingerprintInscreen::onHideFODView() {
     this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+#if FOD_HBM
     set(HBM_ENABLE_PATH, 0);
+#endif
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -151,14 +157,19 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
 }
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
+#if FOD_HBM
     int dimAmount = get(DIM_AMOUNT_PATH, 0);
+#else
+    int dimAmount = 0;
+#endif
+
     LOG(INFO) << "dimAmount = " << dimAmount;
 
     return dimAmount;
 }
 
 Return<bool> FingerprintInscreen::shouldBoostBrightness() {
-    return false;
+    return !FOD_HBM;
 }
 
 Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& callback) {
--- soong/fod.go
+++ soong/fod.go
@@ -3,6 +3,7 @@ package msmnile
 import (
     "android/soong/android"
     "android/soong/cc"
+    "strconv"
     "strings"
 )
 
@@ -10,11 +11,16 @@ func fodFlags(ctx android.BaseContext) []string {
     var cflags []string
 
     var config = ctx.AConfig().VendorConfig("ONEPLUS_MSMNILE_FOD")
+    var hbm = strconv.FormatBool(config.Bool("HBM"))
     var posX = strings.TrimSpace(config.String("POS_X"))
     var posY = strings.TrimSpace(config.String("POS_Y"))
     var size = strings.TrimSpace(config.String("SIZE"))
 
-    cflags = append(cflags, "-DFOD_POS_X=" + posX, "-DFOD_POS_Y=" + posY, "-DFOD_SIZE=" + size)
+    cflags = append(cflags,
+            "-DFOD_HBM=" + hbm,
+            "-DFOD_POS_X=" + posX,
+            "-DFOD_POS_Y=" + posY,
+            "-DFOD_SIZE=" + size)
     return cflags
 }
 

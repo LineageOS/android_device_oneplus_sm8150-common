a4275a80093770134baed537996ac7b22337b7aa
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-31 11:57:48 +0900

    sm8150-common: doze: Introduce raise to wake gesture
    * Highly inspired by iOS raise to wake
    
    - Adapted to OnePlus sm8150 doze
    
    sm8150-common: doze: Add min interval for raise2wake
    
    * avoid having early wakeup
    * also fix typo in acquire timeout
    
    sm8150-common: doze: Enable pickup when raise to wake is toggled
    
    - This is needed in order for raise to wake to function properly.
    
    sm8150-common: doze: Fix pickup status for raise to wake
    
    Co-authored-by: AnierinB <anierinb@evolution-x.org>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I5df0c4f11f1b24ab813abc393960c5f03f5fab1f

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -41,7 +41,7 @@ public final class Utils {
 
     protected static final String CATEG_PICKUP_SENSOR = "pickup_sensor";
 
-    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
+    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up_type";
 
     protected static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
@@ -93,13 +93,14 @@ public final class Utils {
         return new AmbientDisplayConfiguration(context).alwaysOnAvailable();
     }
 
-    protected static boolean isGestureEnabled(Context context, String gesture) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean(gesture, false);
+    protected static boolean isPickUpEnabled(Context context) {
+        return !PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(GESTURE_PICK_UP_KEY, "0").equals("0");
     }
 
-    protected static boolean isPickUpEnabled(Context context) {
-        return isGestureEnabled(context, GESTURE_PICK_UP_KEY);
+    protected static boolean isPickUpSetToWake(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(GESTURE_PICK_UP_KEY, "0").equals("2");
     }
 
     public static boolean areGesturesEnabled(Context context) {

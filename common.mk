494cde42402d4138cd7ebc18433773b8df53a118
LuK1337 <priv.luk@gmail.com>
2020-01-31 16:54:58 +0100

    sm8150-common: Workaround soong namespacing issue
    Change-Id: I2d93be5b1b560d33fbf0454da5c6b036343dc14e

--- common.mk
+++ common.mk
@@ -252,7 +252,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_msmnile \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile \
-    vendor.oneplus.fingerprint.extension@1.0.vendor
+    vendor.oneplus.fingerprint.extension@1.0.vendor \
+    vendor.oneplus.hardware.display@1.0.vendor
 
 # For config.fs
 PRODUCT_PACKAGES += \

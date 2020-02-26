d0496566ccd844c2e7955bb04f250589a8d8dc1d
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    sm8150-common: Build and enable fastbootd
    Since OEM fastboot implementation does not allow us to flash
    critical partitions, fastbootd is useful for upgrading firmware.
    
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- common.mk
+++ common.mk
@@ -244,6 +244,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_msmnile \

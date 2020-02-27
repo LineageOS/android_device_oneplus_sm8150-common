c4871a9086542f6f5bf76a4f31a9b72b7a20c31b
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-02-27 12:53:51 +0200

    sm8150-common: fod: Hardcode dim amount for hbm mode(670)
    Change-Id: Ic403cbd69a57d2da78e388cfc31d0aa8dbb0d8e1

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -35,6 +35,7 @@
 
 // This is not a typo by me. It's by OnePlus.
 #define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/op_friginer_print_hbm"
+#define HBM_MODE_PATH "/sys/class/drm/card0-DSI-1/hbm"
 #define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
 
 namespace vendor {
@@ -156,6 +157,13 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
     int dimAmount = get(DIM_AMOUNT_PATH, 0);
+    int hbmMode = get(HBM_MODE_PATH, 0);
+
+    // Always return 42 for hbm mode(670)
+    if (hbmMode == 5) {
+        dimAmount = 42;
+    }
+
     LOG(INFO) << "dimAmount = " << dimAmount;
 
     return dimAmount;

1983c0686391aa97abb56baef4e1251bba385a7e
Mohd Faraz <androiabledroid@gmail.com>
2020-03-15 20:26:07 +0530

    sm8150-common: update sensors hal upto 2.0
    Change-Id: I981eace1aa7e62ca9308268b17508238e6218e26
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- common.mk
+++ common.mk
@@ -411,8 +411,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl.oneplus_msmnile \
-    android.hardware.sensors@1.0-service.oneplus_msmnile \
+    android.hardware.sensors@2.0-impl.oneplus_msmnile \
+    android.hardware.sensors@2.0-service.oneplus_msmnile \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \

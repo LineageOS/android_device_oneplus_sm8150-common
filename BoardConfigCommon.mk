e455b1eae7edde4014061673daee873885269fe0
Ashwin R C <ashwin2001achu@gmail.com>
2020-04-25 04:35:13 +0000

    sm8150-common: bump vendor security patch level
    https://github.com/AndroidDumps/oneplus_oneplus7pro_dump/blob/78116b230a4358f4f926b506fc7878a2f83aeac7/vendor/build.prop#L3
    
    Change-Id: Ibc81ffe98beb28055868a60aa590cb788958a85b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,7 +180,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-09-05
+VENDOR_SECURITY_PATCH := 2020-03-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk

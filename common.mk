3f0fb128fa76993f823bf7624660be9a6946f699
Anthony Stange <stange@google.com>
2020-04-28 12:03:18 -0400

    sm8150-common: update to Multi-HAL 2.1
    Bug: 149758467
    Test: Verify that the new unit tests and subhals in this topic load and
    run on a Pixel device
    
    Change-Id: I275274659816b3a00374fb0b7a93f199874afdcd

--- common.mk
+++ common.mk
@@ -345,7 +345,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     als_correction_service.oneplus_msmnile \
-    android.hardware.sensors@2.0-service.oneplus_msmnile \
+    android.hardware.sensors@2.1-service.oneplus_msmnile \
     libsensorndkbridge \
     sensors.oplus
 

c39203d477c21907b76ed403218001d302a99b0f
LuK1337 <priv.luk@gmail.com>
2020-04-30 22:43:35 +0200

    sm8150-common: Drop obsolete PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS entry
    Change-Id: I2183e4244480208dee23538c153c343e10954c35

--- common.mk
+++ common.mk
@@ -23,8 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # A/B
 AB_OTA_UPDATER := true

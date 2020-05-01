4379a8c16bd6bc138b00a707bfa1102338fba034
LuK1337 <priv.luk@gmail.com>
2020-05-01 11:45:05 +0200

    Revert "sm8150-common: Drop obsolete PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS entry"
    This reverts commit c39203d477c21907b76ed403218001d302a99b0f.
    
    Change-Id: I7ca785fc7ad7f704c7f578ee0604b48e99f61098

--- common.mk
+++ common.mk
@@ -23,7 +23,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true

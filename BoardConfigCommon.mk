c9e2e05bd1bddbefbc1064df926bdb7f88d80d13
Anirudh Gupta <anirudhgupta109@aosip.dev>
2020-05-01 15:38:43 +0530

    sm8150-common: Split vendor_props into {odm,vendor}.prop
    Change-Id: I03a3529ebe36486534e83643bd1d1184de5c0b12
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,9 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno640
 TARGET_USES_QCOM_BSP := true
 
 # Properties
+TARGET_ODM_PROP += $(VENDOR_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
+TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true

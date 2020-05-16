dd09b97bbf6d269b4968c0a0821ef459ef468c02
LuK1337 <priv.luk@gmail.com>
2020-05-16 12:52:40 +0200

    sm8150-common: fod: Uprev to 1.1
    Change-Id: Ifc5ef199ca6bb8e6176e768e70228adf2103185a

--- common.mk
+++ common.mk
@@ -247,7 +247,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_msmnile \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.1-service.oneplus_msmnile \
     vendor.oneplus.fingerprint.extension@1.0.vendor \
     vendor.oneplus.hardware.display@1.0.vendor
 

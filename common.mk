0e6a4c8f53064a7bf37d3428b6d6018bf4ef032c
Erfan Abdi <erfangplus@gmail.com>
2020-05-21 02:14:58 +0430

    sm8150-common: livedisplay: msm8998 -> msmnile
    * Also add it to build packages
    
    Change-Id: Ib6d8dd67cd8df6cd5f2a1d47ab09d75e540a8156

--- common.mk
+++ common.mk
@@ -312,7 +312,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service.oneplus_msmnile
 
 # Media
 PRODUCT_COPY_FILES += \

739426ffeda7edb6895c5c6af7d5760071d9dfa4
LuK1337 <priv.luk@gmail.com>
2020-06-30 15:46:42 +0200

    sm8150-common: touch: Sync available gestures with kernel
    Change-Id: I203b24d67293f5b124978c59ba5561fbf45cb718

--- touch/TouchscreenGesture.cpp
+++ touch/TouchscreenGesture.cpp
@@ -28,14 +28,14 @@ namespace implementation {
 
 const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGestureInfoMap = {
     {0, {251, "Two fingers down swipe", "/proc/touchpanel/double_swipe_enable"}},
-    {1, {254, "Right arrow", "/proc/touchpanel/right_arrow_enable"}},
-    {2, {255, "Down arrow", "/proc/touchpanel/down_arrow_enable"}},
-    {3, {253, "Left arrow", "/proc/touchpanel/left_arrow_enable"}},
+    {1, {252, "Down arrow", "/proc/touchpanel/down_arrow_enable"}},
+    {2, {253, "Left arrow", "/proc/touchpanel/left_arrow_enable"}},
+    {3, {254, "Right arrow", "/proc/touchpanel/right_arrow_enable"}},
     {4, {247, "Letter M", "/proc/touchpanel/letter_m_enable"}},
     {5, {250, "Letter O", "/proc/touchpanel/letter_o_enable"}},
     {6, {248, "Letter S", "/proc/touchpanel/letter_s_enable"}},
     {7, {246, "Letter W", "/proc/touchpanel/letter_w_enable"}},
-    {8, {67, "Single Tap", "/proc/touchpanel/single_tap_enable"}},
+    {8, {255, "Single Tap", "/proc/touchpanel/single_tap_enable"}},
 };
 
 Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {

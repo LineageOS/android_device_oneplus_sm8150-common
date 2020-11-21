78e0398b35cadcf4e2d15d1804344f2e85093910
LuK1337 <priv.luk@gmail.com>
2020-11-21 14:53:53 +0100

    sm8150-common: Add local OpFeatures implementation
    Change-Id: Ibb1d890a96683e005520da977f2433f8094b0644

--- common.mk
+++ common.mk
@@ -384,6 +384,13 @@ PRODUCT_PACKAGES += \
     libOmxVidcCommon \
     libstagefrighthw
 
+# OnePlus
+PRODUCT_PACKAGES += \
+    oneplus-fwk.oneplus_msmnile
+
+PRODUCT_BOOT_JARS += \
+    oneplus-fwk.oneplus_msmnile
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \

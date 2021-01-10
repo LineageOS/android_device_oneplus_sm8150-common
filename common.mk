7c967af27b487dee03c998895bcc10a4907733d6
Lup Gabriel <gwolf2u.website@gmail.com>
2021-01-10 09:57:06 +0200

    sm8150-common: switch to crDroid doze package

--- common.mk
+++ common.mk
@@ -228,10 +228,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    OnePlusDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey

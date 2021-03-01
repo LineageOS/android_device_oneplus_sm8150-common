9b44d32e92a025d0e61f167d3ec9a41099e745b0
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-03-01 14:28:29 +0800

    sm8150-common: Enable freeform windows
    Change-Id: I94f4f6661c493a7e2606046e669f3528e5bde176

--- common.mk
+++ common.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \

90d43ccf6c81b68db65d37100fb5542ecf8487dc
LuK1337 <priv.luk@gmail.com>
2021-04-01 15:00:27 +0200

    sm8150-common: sensors: Bring back naruto light support
    Also drop full treble flag as we are no longer
    violating Treble SELinux tests.
    
    Change-Id: If6802dc13c3ac56fa2e5cd032f3daad71958bffb

--- common.mk
+++ common.mk
@@ -414,6 +414,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    als_correction_service.oneplus_msmnile \
     android.hardware.sensors@2.0-service.oneplus_msmnile \
     libsensorndkbridge
 

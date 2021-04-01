0ff9a3c9d2fac196687b147b93f54a49a785a53e
LuK1337 <priv.luk@gmail.com>
2021-04-01 15:00:27 +0200

    sm8150-common: sensors: Bring back naruto light support
    Also drop full treble flag as we are no longer
    violating Treble SELinux tests.
    
    Change-Id: If6802dc13c3ac56fa2e5cd032f3daad71958bffb

---
+++ sensors/AlsCorrection.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AlsCorrection.h"
+
+#include <cutils/properties.h>
+#include <fstream>
+#include <log/log.h>
+#include <time.h>
+
+namespace android {
+namespace hardware {
+namespace sensors {
+namespace V2_1 {
+namespace implementation {
+
+static int red_max_lux, green_max_lux, blue_max_lux, white_max_lux, max_brightness;
+static int als_bias;
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+void AlsCorrection::init() {
+    red_max_lux = get("/mnt/vendor/persist/engineermode/red_max_lux", 0);
+    green_max_lux = get("/mnt/vendor/persist/engineermode/green_max_lux", 0);
+    blue_max_lux = get("/mnt/vendor/persist/engineermode/blue_max_lux", 0);
+    white_max_lux = get("/mnt/vendor/persist/engineermode/white_max_lux", 0);
+    als_bias = get("/mnt/vendor/persist/engineermode/als_bias", 0);
+    max_brightness = get("/sys/class/backlight/panel0-backlight/max_brightness", 255);
+    ALOGV("max r = %d, max g = %d, max b = %d", red_max_lux, green_max_lux, blue_max_lux);
+}
+
+void AlsCorrection::correct(float& light) {
+    pid_t pid = property_get_int32("vendor.sensors.als_correction.pid", 0);
+    if (pid != 0) {
+        kill(pid, SIGUSR1);
+    }
+    uint8_t r = property_get_int32("vendor.sensors.als_correction.r", 0);
+    uint8_t g = property_get_int32("vendor.sensors.als_correction.g", 0);
+    uint8_t b = property_get_int32("vendor.sensors.als_correction.b", 0);
+    ALOGV("Screen Color Above Sensor: %d, %d, %d", r, g, b);
+    ALOGV("Original reading: %f", light);
+    int screen_brightness = get("/sys/class/backlight/panel0-backlight/brightness", 0);
+    float correction = 0.0f;
+    if (red_max_lux > 0 && green_max_lux > 0 && blue_max_lux > 0 && white_max_lux > 0) {
+        uint8_t rgb_min = std::min({r, g, b});
+        correction += ((float) rgb_min) / 255.0f * ((float) white_max_lux);
+        r -= rgb_min;
+        g -= rgb_min;
+        b -= rgb_min;
+        correction += ((float) r) / 255.0f * ((float) red_max_lux);
+        correction += ((float) g) / 255.0f * ((float) green_max_lux);
+        correction += ((float) b) / 255.0f * ((float) blue_max_lux);
+        correction = correction * (((float) screen_brightness) / ((float) max_brightness));
+        correction += als_bias;
+    }
+    ALOGV("Final correction: %f", correction);
+    // Sensor is not accurate for low values
+    if (light < correction) {
+        light = correction;
+    }
+}
+
+}  // namespace implementation
+}  // namespace V2_1
+}  // namespace sensors
+}  // namespace hardware
+}  // namespace android

4d69d470047c0e2e74afddf997518c0a663a97c4
LuK1337 <priv.luk@gmail.com>
2021-04-04 17:38:33 +0200

    sm8150-common: Remove android.hidl.{base,manager}
    No longer used.
    
    Change-Id: I114f44c4fd398c954e8a6cc72863159e3cec50bf

--- common.mk
+++ common.mk
@@ -272,10 +272,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \

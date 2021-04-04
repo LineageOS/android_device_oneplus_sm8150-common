7c3fbe6ce8da730ee81032164a7117ddca5e3be5
LuK1337 <priv.luk@gmail.com>
2021-04-04 17:54:54 +0200

    sm8150-common: fod: Update LOG_TAG
    Change-Id: I0ce3291f6af18da5fa684304fdff362a93669c7f

--- fod/service.cpp
+++ fod/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile"
+#define LOG_TAG "vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>

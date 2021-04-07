2e1f67d37a155ed91bb3014d0dbb1cc33c54f901
LuK1337 <priv.luk@gmail.com>
2021-04-07 11:41:02 +0200

    sm8150-common: Vendor tree is not optional
    Change-Id: I14116595bedf7545036c62d62d1e14f6d4dbd06c

--- common.mk
+++ common.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
+$(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \

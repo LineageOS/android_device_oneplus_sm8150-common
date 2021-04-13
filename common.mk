449fbc7053eae50af25f6e363c25e6954740eb2b
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    sm8150-common: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- common.mk
+++ common.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \

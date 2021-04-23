714d59fff5c202152cffdc2b68ef0d16099088e2
LuK1337 <priv.luk@gmail.com>
2021-04-23 20:16:46 +0200

    sm8150-common: Remove libchrome from PRODUCT_PACKAGES
    We are using prebuilt one.
    
    Change-Id: Ide7d3470ec29167279d1f54b593c05b5372e475a

--- common.mk
+++ common.mk
@@ -311,11 +311,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Misc
-PRODUCT_PACKAGES += \
-    libchrome \
-    libchrome.vendor
-
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0

f151c3b38e2bc9be7904da27a542a06c73e8c6a6
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-05-26 17:57:48 +0530

    sm8150-common: sensors: Apply ALS correction to the RAW sensor value
    * Probably a derp here https://github.com/LineageOS/android_device_oneplus_sm8150-common/commit/07c6e1a71fa7ef5ef58561c86097ef5e7ea10934 that led to NOT using the correction at all
    
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I8508ed0db60a70fbb4fbea2850e37ceaa894ca50

--- sensors/AlsCorrection.cpp
+++ sensors/AlsCorrection.cpp
@@ -73,11 +73,15 @@ void AlsCorrection::correct(float& light) {
         correction = correction * (((float) screen_brightness) / ((float) max_brightness));
         correction += als_bias;
     }
-    ALOGV("Final correction: %f", correction);
-    // Sensor is not accurate for low values
-    if (light < correction) {
+    // Do not apply correction if < 0, prevent unstable adaptive brightness
+    if (light - correction >= 0) {
+        light -= correction;
+    } else {
+        // In low light conditions, sensor is just reporting bad values, using
+        // computed correction instead allows to fix the issue
         light = correction;
     }
+    ALOGV("Corrected reading: %f", light);
 }
 
 }  // namespace implementation

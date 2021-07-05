c5879714000849db266ee3fb29e4f33c1296ea76
LuK1337 <priv.luk@gmail.com>
2021-07-05 22:27:17 +0200

    sm8150-common: wifi-mac-generator: Use serialno for generating WLAN MAC
    Since RIL is no longer dumping NV items on boot we are always falling
    back to random MAC address. This change makes it at least stable...
    
    Change-Id: I80edb7671228a0f34077d8a226b6dd66c8cbe94a

--- wifi-mac-generator/wifi-mac-generator.sh
+++ wifi-mac-generator/wifi-mac-generator.sh
@@ -19,10 +19,13 @@ function wait_for_file() {
 }
 
 if [[ ! -f "${WLAN_MAC_PERSIST_PATH}" ]] || [[ ! -s "${WLAN_MAC_PERSIST_PATH}" ]]; then
+    SERIALNO=`printf '%d' $((16#$1))`
+
     MAC_0_PATH="/data/vendor/oemnvitems/4678_0"
 
     if ! wait_for_file "${MAC_0_PATH}"; then
-        MAC_0="${WLAN_MAC_VENDOR_PREFIX}`xxd -l 3 -p /dev/urandom | tr '[:lower:]' '[:upper:]'`"
+        MAC_0_SUFFIX=`awk -v seed=${SERIALNO} -v min=0 -v max=255 'BEGIN{srand(seed); printf("%02x%02x%02x", int(min+rand()*(max-min+1)), int(min+rand()*(max-min+1)), int(min+rand()*(max-min+1)))}' | tr '[:lower:]' '[:upper:]'`
+        MAC_0="${WLAN_MAC_VENDOR_PREFIX}${MAC_0_SUFFIX}"
     else
         MAC_0=`xxd -p "${MAC_0_PATH}" | grep -o '..' | tac | tr -d '\n' | tr '[:lower:]' '[:upper:]'`
     fi
@@ -30,7 +33,8 @@ if [[ ! -f "${WLAN_MAC_PERSIST_PATH}" ]] || [[ ! -s "${WLAN_MAC_PERSIST_PATH}" ]
     MAC_1_PATH="/data/vendor/oemnvitems/4678_1"
 
     if ! wait_for_file "${MAC_1_PATH}"; then
-        MAC_1="${WLAN_MAC_VENDOR_PREFIX}`xxd -l 3 -p /dev/urandom | tr '[:lower:]' '[:upper:]'`"
+        MAC_1_SUFFIX=`awk -v seed=$((${SERIALNO}+1)) -v min=0 -v max=255 'BEGIN{srand(seed); printf("%02x%02x%02x", int(min+rand()*(max-min+1)), int(min+rand()*(max-min+1)), int(min+rand()*(max-min+1)))}' | tr '[:lower:]' '[:upper:]'`
+        MAC_1="${WLAN_MAC_VENDOR_PREFIX}${MAC_1_SUFFIX}"
     else
         MAC_1=`xxd -p "${MAC_1_PATH}" | grep -o '..' | tac | tr -d '\n' | tr '[:lower:]' '[:upper:]'`
     fi

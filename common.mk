559171742599f28da003e09dc6210142a994562c
LuK1337 <priv.luk@gmail.com>
2021-07-10 15:24:13 +0200

    sm8150-common: Call oem_qmi_access_func() on RIL init
    This makes us dump NV items on boot which in the end fixes invalid MAC
    address for BT and WLAN.
    
    This change should be reverted as soon as RIL stack is fixed.
    
    Change-Id: I4a2756c91230f43d0c9528d1a685aa7c90723590

--- common.mk
+++ common.mk
@@ -370,6 +370,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
+    libril_shim \
     librmnetctl \
     libxml2
 

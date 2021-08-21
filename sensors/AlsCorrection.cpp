fbb507eaff8aec0a2b5d3d4bd116892940706a39
dantmnf <dantmnf2@gmail.com>
2021-08-21 17:53:34 +0800

    sm8150-common: use average linear light for ALS correction
    ALS calibration values from my persist partition don't look nice.
    
    Prefer calibration from another persist location (not exist on stock)
    or properties.
    
    Change-Id: Ia784480462f5a236d0bd75861f1cf5fd170fe3b1

--- sensors/AlsCorrection.cpp
+++ sensors/AlsCorrection.cpp
@@ -17,6 +17,7 @@
 #include "AlsCorrection.h"
 
 #include <cutils/properties.h>
+#include <optional>
 #include <fstream>
 #include <log/log.h>
 #include <time.h>
@@ -30,6 +31,15 @@ namespace implementation {
 static int red_max_lux, green_max_lux, blue_max_lux, white_max_lux, max_brightness;
 static int als_bias;
 
+static std::optional<int32_t> property_get_opt(const char *key) {
+    char buf[PROPERTY_VALUE_MAX] = {};
+    int len = property_get(key, buf, "");
+    if (len) {
+        return atoi(buf);
+    }
+    return {};
+}
+
 template <typename T>
 static T get(const std::string& path, const T& def) {
     std::ifstream file(path);
@@ -39,13 +49,36 @@ static T get(const std::string& path, const T& def) {
     return file.fail() ? def : result;
 }
 
+template <typename T>
+static std::optional<T> get(const std::string& path) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    if (file.fail()){
+        return {};
+    } else {
+        return {result};
+    }
+}
+
 void AlsCorrection::init() {
-    red_max_lux = get("/mnt/vendor/persist/engineermode/red_max_lux", 0);
-    green_max_lux = get("/mnt/vendor/persist/engineermode/green_max_lux", 0);
-    blue_max_lux = get("/mnt/vendor/persist/engineermode/blue_max_lux", 0);
-    white_max_lux = get("/mnt/vendor/persist/engineermode/white_max_lux", 0);
-    als_bias = get("/mnt/vendor/persist/engineermode/als_bias", 0);
-    max_brightness = get("/sys/class/backlight/panel0-backlight/max_brightness", 255);
+    red_max_lux = get<int32_t>("/mnt/vendor/persist/als_correction.lineage/red_max_lux").value_or(
+                    property_get_opt("ro.vendor.sensors.als_correction_defaults.red_max_lux").value_or(
+                        get("/mnt/vendor/persist/engineermode/red_max_lux", 0)));
+    green_max_lux = get<int32_t>("/mnt/vendor/persist/als_correction.lineage/green_max_lux").value_or(
+                        property_get_opt("ro.vendor.sensors.als_correction_defaults.green_max_lux").value_or(
+                            get("/mnt/vendor/persist/engineermode/green_max_lux", 0)));
+    blue_max_lux = get<int32_t>("/mnt/vendor/persist/als_correction.lineage/blue_max_lux").value_or(
+                        property_get_opt("ro.vendor.sensors.als_correction_defaults.blue_max_lux").value_or(
+                            get("/mnt/vendor/persist/engineermode/blue_max_lux", 0)));
+    white_max_lux = get<int32_t>("/mnt/vendor/persist/als_correction.lineage/white_max_lux").value_or(
+                        property_get_opt("ro.vendor.sensors.als_correction_defaults.white_max_lux").value_or(
+                            get("/mnt/vendor/persist/engineermode/white_max_lux", 0)));
+    als_bias = get<int32_t>("/mnt/vendor/persist/als_correction.lineage/als_bias").value_or(
+                    property_get_opt("ro.vendor.sensors.als_correction_defaults.als_bias").value_or(
+                        get("/mnt/vendor/persist/engineermode/als_bias", 0)));
+    max_brightness = property_get_opt("").value_or(get("/sys/class/backlight/panel0-backlight/max_brightness", 255));
     ALOGV("max r = %d, max g = %d, max b = %d", red_max_lux, green_max_lux, blue_max_lux);
 }
 
@@ -54,22 +87,24 @@ void AlsCorrection::correct(float& light) {
     if (pid != 0) {
         kill(pid, SIGUSR1);
     }
-    uint8_t r = property_get_int32("vendor.sensors.als_correction.r", 0);
-    uint8_t g = property_get_int32("vendor.sensors.als_correction.g", 0);
-    uint8_t b = property_get_int32("vendor.sensors.als_correction.b", 0);
+    // TODO: HIDL service and pass float instead
+    int32_t r = property_get_int32("vendor.sensors.als_correction.r", 0);
+    int32_t g = property_get_int32("vendor.sensors.als_correction.g", 0);
+    int32_t b = property_get_int32("vendor.sensors.als_correction.b", 0);
     ALOGV("Screen Color Above Sensor: %d, %d, %d", r, g, b);
     ALOGV("Original reading: %f", light);
     int screen_brightness = get("/sys/class/backlight/panel0-backlight/brightness", 0);
     float correction = 0.0f;
     if (red_max_lux > 0 && green_max_lux > 0 && blue_max_lux > 0 && white_max_lux > 0) {
-        uint8_t rgb_min = std::min({r, g, b});
-        correction += ((float) rgb_min) / 255.0f * ((float) white_max_lux);
+        constexpr float rgb_scale = 0x7FFFFFFF;
+        uint16_t rgb_min = std::min({r, g, b});
+        correction += ((float) rgb_min) / rgb_scale * ((float) white_max_lux);
         r -= rgb_min;
         g -= rgb_min;
         b -= rgb_min;
-        correction += ((float) r) / 255.0f * ((float) red_max_lux);
-        correction += ((float) g) / 255.0f * ((float) green_max_lux);
-        correction += ((float) b) / 255.0f * ((float) blue_max_lux);
+        correction += ((float) r) / rgb_scale * ((float) red_max_lux);
+        correction += ((float) g) / rgb_scale * ((float) green_max_lux);
+        correction += ((float) b) / rgb_scale * ((float) blue_max_lux);
         correction = correction * (((float) screen_brightness) / ((float) max_brightness));
         correction += als_bias;
     }

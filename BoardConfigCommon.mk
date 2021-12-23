9d3c8b5cdfa26a1ef17196d4ea143f3bd4944f31
LuK1337 <priv.luk@gmail.com>
2021-12-23 16:43:36 +0100

    sm8150-common: Update blobs from OOS 11.0.5.1
    Change-Id: I8fd6a4d8575d56a59f4efe4d640719d046ff2109

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,7 +139,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-10-01
+VENDOR_SECURITY_PATCH := 2021-12-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk

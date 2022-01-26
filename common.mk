6d7532fa41f8b96d73dd7e0926343ccc6fcadbd0
LuK1337 <priv.luk@gmail.com>
2022-01-26 20:37:59 +0100

    sm8150-common: Update WFD system stack from lemonadep
    Change-Id: I728810877f3aa40a4fe068f23fd2cfc9194f8066

--- common.mk
+++ common.mk
@@ -208,8 +208,9 @@ PRODUCT_PACKAGES += \
     gralloc.msmnile \
     hwcomposer.msmnile \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
+    libdisplayconfig.system.qti \
     libqdMetaData \
+    libqdMetaData.system \
     libtinyxml \
     libvulkan \
     memtrack.msmnile \

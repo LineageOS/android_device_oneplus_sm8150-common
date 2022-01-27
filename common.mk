00c95028da1889ca75af5452d85e0bfac449d22d
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    sm8150-common: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- common.mk
+++ common.mk
@@ -117,7 +117,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    Snap \
     vendor.oneplus.camera.CameraHIDL@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 

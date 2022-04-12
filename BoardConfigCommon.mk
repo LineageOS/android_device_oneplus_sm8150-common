d977120ab69dbda2bcae9cf2e78a794da6f5e8dc
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 21:13:13 -0700

    sm8150-common: Sort kernel cmdline and additional flags as a list
     * This allows better readability and eases tracking further changes.
    
    Change-Id: I3a23f7ccb238b5f0e18dfa18080f1ae8ff51109f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,8 +84,18 @@ ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.vbmeta.avb_version=1.0 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048 \
+    video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_IMAGE_NAME := Image-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true

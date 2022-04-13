565ed7f39107d0c6d44518bfec997e5f2fb19134
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 17:16:55 -0700

    sm8150-common: Sync properties with OOS
    Change-Id: I7b53d83d9acd46fd91f842f552338952a95f86d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,7 +50,8 @@ TARGET_NO_BOOTLOADER := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Bluetooth

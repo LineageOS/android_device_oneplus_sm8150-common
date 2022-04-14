1196fb4916459a08ab2cf831528951f486e1381b
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:30:41 -0700

    sm8150-common: overlay: Atomize
    Change-Id: I972d46e1dafb03b42249193fd033e91c4e3b3bc5

--- common.mk
+++ common.mk
@@ -308,12 +308,9 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_PACKAGES += \
-    WifiResCommon
 
 # Power
 PRODUCT_PACKAGES += \

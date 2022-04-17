dbfc7807c143735ee636111666673cf5357c125a
Alexander Koskovich <akoskovich@pm.me>
2022-04-17 16:14:07 -0700

    sm8150-common: Move default.prop properties to vendor.prop
    Change-Id: I91759cc0a37b504dd5f9a2bf978c9b207da02435

--- common.mk
+++ common.mk
@@ -438,15 +438,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oneplus
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.wcg_composition_dataspace=143261696
-
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \

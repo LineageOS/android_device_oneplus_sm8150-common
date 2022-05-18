4cc43f3f0ea937f32a6f6d52680630102ff46831
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 05:29:56 -0700

    sm8150-common: Pick up QSSI overlays from hardware/oplus
    Change-Id: I796c4ca9939a70f7917303d509a51dba150f5174

--- common.mk
+++ common.mk
@@ -304,6 +304,8 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
+$(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
+
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 

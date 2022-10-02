f7a98dd12bcdd3c9352d99742b0115c818e291fc
LuK1337 <priv.luk@gmail.com>
2022-10-02 23:05:04 +0200

    sm8150-common: sepolicy: Add rules for camera helper app
    Change-Id: Ia7526a4342a1f08165e029b19426532adef9c780

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -4,3 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public

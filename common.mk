e5291fc308d640460e1cdbba55441461335e1d2d
LuK1337 <priv.luk@gmail.com>
2022-11-25 15:46:37 +0100

    sm8150-common: Use libcamera_metadata_shim for unlocking cameras
    Change-Id: I985c6e317b36d8cdc6b41a9f9f625b293a8987eb

--- common.mk
+++ common.mk
@@ -106,6 +106,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera_metadata_shim \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 

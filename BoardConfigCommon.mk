fffe3f945c0df6ae87fbb5cf55a478f7f150f24f
LuK1337 <priv.luk@gmail.com>
2023-12-03 16:17:00 +0100

    sm8150-common: Include the proprietary files BoardConfig
    Change-Id: I962047e1f94a5cfb12bda2f114cb88fe960b4645

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,3 +177,6 @@ WIFI_DRIVER_STATE_ON := "ON"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Include the proprietary files BoardConfig.
+include vendor/oneplus/sm8150-common/BoardConfigVendor.mk

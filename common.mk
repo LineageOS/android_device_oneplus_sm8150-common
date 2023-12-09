084a82823cbcd2a9dfc2ce454d0aca82bdd7d173
Georg Veichtlbauer <georg@vware.at>
2023-12-09 16:40:36 +0100

    sm8150-common: Inherit general overlays from OPlus
    Change-Id: I7ecb6c9ea6636450076918bef6dce9986342ae3e

--- common.mk
+++ common.mk
@@ -303,7 +303,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-$(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
+$(call inherit-product, hardware/oplus/overlay/overlay.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage

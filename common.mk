f8cc8453c70b1fd27a97261096508e0e1554c786
Georg Veichtlbauer <georg@vware.at>
2024-01-26 10:49:44 +0100

    sm8150-common: Switch to power-libperfmgr
    Nick powerhint.json from Coral @ aaf71b7
    And build dummy libqti-perfd-client
    
    Change-Id: I98748776140d3158645ac5f0ba23b333731c7dd6

--- common.mk
+++ common.mk
@@ -317,10 +317,14 @@ PRODUCT_PACKAGES += \
     OPlusSystemUIResCommon
 
 # Power
+$(call inherit-product, hardware/oplus/libqti-perfd-client/libqti-perfd-client.mk)
+$(call inherit-product, hardware/oplus/power-libperfmgr/power-libperfmgr.mk)
+
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power@1.2.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \

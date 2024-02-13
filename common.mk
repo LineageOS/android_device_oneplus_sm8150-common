43ed0739faa5f7d571a86439b7c2f8ab53f90c9d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-13 11:23:49 +0800

    sm8150-common: Build QTI Thermal 2.0 HAL
    Change-Id: Iae471a16a0284cd24a598bac1a3ea0736faceb9f

--- common.mk
+++ common.mk
@@ -309,6 +309,10 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oplus

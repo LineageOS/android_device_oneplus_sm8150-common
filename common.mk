92f4dbb297d872db244c1a3d728e69225b9ee02d
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:59:13 +0300

    sm8150-common: Drop libvulkan target that is built by default
    Change-Id: Ic5d73508b850fac7c7427caf12eb4a8e87e3deca

--- common.mk
+++ common.mk
@@ -117,7 +117,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     gralloc.qcom \
     hwcomposer.qcom \
-    libvulkan \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.memtrack-service
 

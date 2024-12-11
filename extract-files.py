59ba31859e03633d603b2f7fc6dd7da0b9427e85
Nick Reuter <nreuter85@gmail.com>
2024-12-10 20:32:13 -0600

    sm8150-common: Address missing libbinder symbols in libwfdnative
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder10atrace_endEm
    
    Change-Id: I942d51721461dbbf9c8d0f2f32128529bb7480c3

--- extract-files.py
+++ extract-files.py
@@ -67,6 +67,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
+        .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0x00'),

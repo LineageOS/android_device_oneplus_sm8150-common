43573bc391ab3e2bddcfdddbcd86124ab3c40e53
LuK1337 <priv.luk@gmail.com>
2025-01-31 10:28:03 +0100

    sm8150-common: Move media_codecs_c2.xml to device tree
    Change-Id: Ib2ebfff4281eafd46427180c2666a71d0d144300

--- common.mk
+++ common.mk
@@ -206,6 +206,7 @@ $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_DM,true)
 
 # Media
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
 
 # NFC

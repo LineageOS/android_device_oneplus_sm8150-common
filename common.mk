cc9b6503d28e483416b4196d579c67ad23c2a651
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sm8150-common: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- common.mk
+++ common.mk
@@ -282,6 +282,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
+    hardware/qcom-caf/bootctrl \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony

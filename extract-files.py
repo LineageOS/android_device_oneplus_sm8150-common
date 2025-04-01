c81502cc21aaf3278eeadf94e79f3f2b8afac1e3
LuK1337 <priv.luk@gmail.com>
2025-04-01 16:11:23 +0200

    sm8150-common: Add libwfdservice_shim_v1 for WFD
    e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I60691f2bab3f52fd9ade36ed6fcf6a2ef13a18c9

--- extract-files.py
+++ extract-files.py
@@ -60,6 +60,8 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('blob-patches/PowerOffAlarm.patch', '-s'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
         .regex_replace('/my_product', '/product'),
+    'system_ext/bin/wfdservice': blob_fixup()
+        .add_needed('libwfdservice_shim_v1.so'),
     'system_ext/framework/oplus-ims-ext.jar': blob_fixup()
         .apktool_patch('blob-patches/oplus-ims-ext.patch', '-r'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()

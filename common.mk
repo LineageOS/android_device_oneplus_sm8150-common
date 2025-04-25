38380ffcb2bbdbd41c4a8d10b3896d12fc45e08f
Bruno Martins <bgcngm@gmail.com>
2025-04-25 17:36:15 +0100

    sm8150-common: Switch to Touch AIDL
    Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

--- common.mk
+++ common.mk
@@ -319,8 +319,9 @@ $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
+    vendor.lineage.touch-service.oplus
 
+$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
 
 # Update engine

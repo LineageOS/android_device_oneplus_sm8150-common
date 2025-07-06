876f6709cc461b387ac2c9ceb1d640b2579592c2
LuK1337 <priv.luk@gmail.com>
2025-07-06 12:54:55 +0200

    sm8150 apktool_patch() no longer takes custom arguments
    Change-Id: Idf85ae2cd0505c34e55f0e6acec2afc085372b78

--- extract-files.py
+++ extract-files.py
@@ -63,7 +63,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/bin/wfdservice': blob_fixup()
         .add_needed('libwfdservice_shim.so'),
     'system_ext/framework/oplus-ims-ext.jar': blob_fixup()
-        .apktool_patch('blob-patches/oplus-ims-ext.patch', '-r'),
+        .apktool_patch('blob-patches/oplus-ims-ext.patch'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()

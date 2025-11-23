ea66027efa7831dd2c465ebcc3b9dee1c8214ba3
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    sm8150-common: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -62,6 +62,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     'system_ext/framework/oplus-ims-ext.jar': blob_fixup()
         .apktool_patch('blob-patches/oplus-ims-ext.patch'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0x00'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()

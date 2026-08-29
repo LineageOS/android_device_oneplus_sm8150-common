cea2bc36b3586b7626e04b1f498db8ee90c06608
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    sm8150-common: Drop UFFD flags
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- common.mk
+++ common.mk
@@ -197,7 +197,6 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set_bool,libion,legacy_impl,true)
 
 # Kernel
-PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Lineage Health
